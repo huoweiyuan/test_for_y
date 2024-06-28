4ced0943390344a5fee9b41b89f4e346c29a2f94
manghaode <manghaode@outlook.com>
2024-06-28 16:27:35 +0000

    delete compile files

--- build_vs/y/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/huoweiyuan/projects/test_for_y/y
-# Build directory: /Users/huoweiyuan/projects/test_for_y/build_vs/y
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
